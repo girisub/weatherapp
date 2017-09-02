ac127da48c42ccb54f1a6306d592d1361f7bf574
Giridhar <giri.psbb@gmail.com>
2017-09-02 13:47:14 +0800

    Small changes

--- js/weather.js
+++ js/weather.js
@@ -1,30 +1,38 @@
-var x = document.getElementById("demo");
+
+window.addEventListener('load',
+function(){
+	var x = document.getElementById("demo");
 	function getLocation() {
-	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(showPosition);
+			if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition(showPosition);
 
-	    } else {
-	        x.innerHTML = "Geolocation is not supported by this browser.";
-	    }
-
-
+			} else {
+					x.innerHTML = "Geolocation is not supported by this browser.";
+			}
 	}
 
 	function showPosition(position) {
-	    x.innerHTML = "Latitude: " + position.coords.latitude +
-	    "<br>Longitude: " + position.coords.longitude;
-	     $.ajax({
+			x.innerHTML = "Latitude: " + position.coords.latitude +
+			"<br>Longitude: " + position.coords.longitude;
+			 $.ajax({
 				type:'GET',
 				url: 'http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+'&lon='+position.coords.longitude+'&APPID=3fe50b72be81a84793b8c0a6b3fa5b78&units=metric',
 				success: function(response){
 					// console.log(response['quote'].body);
 					// console.log(response['quote'].author);
-					// x.innerHTML = "Weather is min temp:" + response['main']['temp_min']
+					x.innerHTML = "Weather is temp:" + response['main']['temp'] +"<br>Country:" + response['sys']['country'] +"<br>Place:"+ response['name']
 					console.log(response['name']);
 					console.log(response['sys']['country']);
 					console.log(response['main']['temp']);
 					// $('#quote').html('<p>'+$quote+'</p><br>'+$author);
 				}
 			});
-	}
-;
+	};
+	getLocation();
+	//showPosition();
+}, false);
+
+
+
+
+// window.onload = load;
